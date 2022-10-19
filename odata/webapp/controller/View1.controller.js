c16189d9516bac10da4943b39c5cfa91677710c2
chanulkim <cu625@naver.com>
2022-10-19 18:02:17 +0900

    20221019 fiori

--- odata/webapp/controller/View1.controller.js
+++ odata/webapp/controller/View1.controller.js
@@ -1,15 +1,97 @@
 sap.ui.define(
     [
         "sap/ui/core/mvc/Controller",
-        "sap/f/library"
+        "sap/ui/model/json/JSONModel",
+        "sap/f/library",
+        "sap/ui/model/Filter",
+        "sap/ui/model/FilterOperator"
         
     ],
-    function(Controller, library) {
+    function(Controller, JSONModel, library,  Filter, FilterOperator) {
       "use strict";
   
       return Controller.extend("odata.controller.View1", {
         onInit: function () {
 
+          var oData = {
+            oTableData : [],
+            sSearchPlant : null,
+            sSearchRoomid  : null,
+            sSearchFloor : null
+          }
+
+          var oDataModel = new JSONModel(oData);
+          this.getView().setModel(oDataModel, "Main");
+
+        },
+
+        
+        onBeforeRendering: function() {
+          var oModel = this.getView().getModel();
+          var oViewModel = this.getView().getModel("Main")
+
+          oModel.read("/RoomstatusSet" , {
+            success: function(oModelData){
+              var data = oModelData; 
+              var aData = data.results;
+              var oData = [];
+
+              oData.push(aData);
+              oViewModel.setProperty("/oTableData", aData);
+
+            }
+          })
+        },
+
+        onSearch: function() {
+          var oViewModel = this.getView().getModel("Main");
+          var sSearchPlant = oViewModel.getProperty("/sSearchPlant");
+          var sSearchPono  = oViewModel.getProperty("/sSearchRoomid");
+          var sSearchSeqno = oViewModel.getProperty("/sSearchFloor");
+
+        
+          var oTable = this.getView().byId("RoomTable"); 
+          var oBinding = oTable.getBinding("items"); 
+
+          var aFilter = [];
+
+          
+          if(sSearchPlant){
+            var oFilter = new Filter ({
+                path : 'Plant', 
+                operator: FilterOperator.Contains,
+                value1: sSearchPlant,
+                caseSensitive : false
+            });
+            aFilter.push(oFilter)
+          }
+
+          if(sSearchPono){
+            var oFilter = new Filter ({
+                path : 'Floor', 
+                operator: FilterOperator.Contains,
+                value1: sSearchFloor,
+                caseSensitive : false
+            });
+            aFilter.push(oFilter)
+          }
+
+          if(sSearchSeqno){
+            var oFilter = new Filter ({
+                path : 'Roomid', 
+                operator: FilterOperator.Contains,
+                value1: sSearchRoomid,
+                caseSensitive : false
+            });
+            aFilter.push(oFilter)
+          }
+
+          oBinding.filter(aFilter);
+
+        },
+
+        onFilterChange: function () {
+          this.onSearch();
         },
 
         onListItemPress: function() {
